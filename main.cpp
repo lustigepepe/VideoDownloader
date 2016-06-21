bcd308378ee0153efe60b43c7c54b774584c4c5c
lustigepepe <lustigepepe@web.de>
2016-06-21 13:29:18 +0200

    so with responseStream from server

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <boost/bind.hpp>
 #include <boost/asio/ssl.hpp>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 using boost::asio::ip::tcp;
@@ -22,6 +23,12 @@ int main(int argc, char *argv[])
     //    return a.exec();
     // Create a context that uses the default paths for
     // finding CA certificates.
+    //    string host("www.google.de");
+    string host("de.wikipedia.org");
+    //    string parameter("/");
+
+    string parameter("/wiki/Katzen");
+
     ssl::context ctx(ssl::context::sslv23);
     //    ctx.set_default_verify_paths();
     ctx.load_verify_file("/usr/local/etc/openssl/certs/cert.pem");
@@ -29,7 +36,7 @@ int main(int argc, char *argv[])
     boost::asio::io_service io_service;
     ssl_socket sock(io_service, ctx);
     tcp::resolver resolver(io_service);
-    tcp::resolver::query query("www.google.de","https");
+    tcp::resolver::query query(host,"https");
     boost::asio::connect(sock.lowest_layer(), resolver.resolve(query));
     sock.lowest_layer().set_option(tcp::no_delay(true));
 
@@ -37,23 +44,58 @@ int main(int argc, char *argv[])
     // certificate.
     //    sock.set_verify_mode(ssl::verify_none);
     sock.set_verify_mode(ssl::verify_peer);
-    sock.set_verify_callback(ssl::rfc2818_verification("www.google.de"));
+    sock.set_verify_callback(ssl::rfc2818_verification(host));
     sock.handshake(ssl_socket::client);
-    tcp::resolver::iterator iterator = resolver.resolve(query);
 
-    string parameter("/");
 
-    tcp::iostream stream;
-    stream.expires_from_now(boost::posix_time::seconds(60));
-    stream.connect("www.google.de", "http");
+
+    //    tcp::iostream stream;
+    //    stream.expires_from_now(boost::posix_time::seconds(60));
+    //    stream.connect(host, "http");
+    boost::asio::streambuf request;
+    ostream stream(&request);
     stream << "GET " << parameter << " HTTP/1.0\r\n";
-    stream << "Host: www.google.de\r\n";
+    stream << "Host:" << host << "\r\n";
     stream << "Accept: */*\r\n";
     stream << "Connection: close\r\n\r\n";
-    stream.flush();
-    std::cout << stream.rdbuf();
+//    stream.flush();
+//    cout << stream.rdbuf();
 
-    cout << "yes man !!" << endl;
-    return 0;
+     size_t t;
+    const char* header=boost::asio::buffer_cast<const char*>(request.data());
+    cout<<header<<endl;
+    try{
+        boost::asio::write(sock, request);
+        stream.clear();
+        size_t t=sizeof(request);
+        request.consume(t);
+    }catch(runtime_error e)
+    {
+        cout << "shit happens man" << endl;
+        cout << e.what() << endl;
+
+    }
+    boost::asio::streambuf response;
+    try{
+        //Point to start next time && and done//
+        boost::asio::read_until(sock, response, "\r\n");
+    }catch(runtime_error e)
+    {
+        cout << e.what() << endl;
+
+    }
+    std::istream response_stream(&response);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    cout<<status_code<<"    status_code"<<endl;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+
+
+
+cout << "yes man !!" << endl;
+return 0;
 
 }
